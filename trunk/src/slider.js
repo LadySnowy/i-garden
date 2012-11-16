36af5eaf12a56398cfd89810897e3699a479a71e
TC.Johnism@gmail.com <TC.Johnism@gmail.com@4e8364fe-7fed-9134-efd9-740170bc0cd8>
2012-11-16 18:32:50 +0000

    function call is makeSlider(arr, itemNum, units, min, max) arr=array with vegetable data itemNum=the item in the array to build the slider for units=the units to display next to the value min=minimum slider value max=maximum slider value

--- trunk/src/slider.js
+++ trunk/src/slider.js
@@ -1,18 +1,20 @@
-	function makeSlider(name)
+	function makeSlider(arr,itemNum,units,min,max)
 	{
-	//create contanier div
+	//create vegitable contanier div
 	var div=document.createElement("div");
-	div.id=name;
-	//make paragraoh for label
+	div.id=arr[itemNum][1];
+	
+	//make paragraph for label
 	var para=document.createElement("p");
 	//make label
 	var lab=document.createElement("label");
-	lab.htmlFor="amount"+name;
-	var node1=document.createTextNode(name+" pounds yeild:");//make this text inout to function
+	lab.htmlFor="amount"+arr[itemNum][1];
+	var node1=document.createTextNode(arr[itemNum][1]+" yeild:");
+	
 	//make input
 	var inp=document.createElement("input");
 	inp.type="text";
-	inp.id="amount"+name;
+	inp.id="amount"+arr[itemNum][1];
 	inp.style.border="0"; 
 	inp.style.color="#f6931f";
 	inp.style.fontWeight="bold";
@@ -24,24 +26,51 @@
 	
 	//make slider div
 	var div1=document.createElement("div");
-	div1.id="slider"+name;
+	div1.id="slider"+arr[itemNum][1];
 	
-	//add everything to the div
+	//make paragraph for min max label
+	var para2=document.createElement("p");
+	
+	//make min label
+	var lab2=document.createElement("label");
+	//lab.htmlFor="amount"+arr[itemNum][1];
+	var node1=document.createTextNode(min);
+	
+	var para3=document.createElement("p");
+	para3.style.textAlign="right";
+	//make max label
+	var lab3=document.createElement("label");	
+	var node2=document.createTextNode(max);
+	
+		
+	lab2.appendChild(node1);
+	lab3.appendChild(node2);
+	
+	para2.appendChild(lab2);
+	para3.appendChild(lab3);
+	
+	div1.appendChild(para2);
+	div1.appendChild(para3);
+	
+	//add everything to the vegitable contanier div
 	div.appendChild(para);
 	div.appendChild(div1);
-
+	
+	//add vegitable contanier to vegList
 	var element=document.getElementById("vegList");
 	element.appendChild(div);
 	
-	$( "#slider"+name ).slider({
+	$( "#slider"+arr[itemNum][1] ).slider({
             //range: true,
-            min: 0,
-            max: 500,
-            value: 300,
-            slide: function( event, ui ) {
-                $( "#amount"+name ).val( ui.value );
+            min: min,
+            max: max,
+            value: arr[itemNum][37],
+            slide: function( event, ui ) {	
+				arr[itemNum][37] = ui.value;
+                $( "#amount"+arr[itemNum][1] ).val(arr[itemNum][37] + " " + units);
+				//( ui.value + " " + units);
             }
         });
-        $( "#amount"+name ).val( $( "#slider"+name ).slider( "values", 0 ) );
+        $( "#amount"+arr[itemNum][1] ).val( $( "#slider"+arr[itemNum][1]).slider( "values", 0 ) + " " + units );
 	}	
 
