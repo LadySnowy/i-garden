58c79c9ecc1c0169ea24db1a4dc0b272b807573b
TC.Johnism@gmail.com <TC.Johnism@gmail.com@4e8364fe-7fed-9134-efd9-740170bc0cd8>
2012-11-18 16:58:49 +0000

    Modified to calculate health of irrigation

--- src/slider.js
+++ src/slider.js
@@ -16,7 +16,7 @@ var inp=document.createElement("input");
 inp.type="text";
 inp.id="amount"+arr[itemNum][1];
 inp.style.border="0"; 
-inp.style.color="#f6931f";
+inp.style.color="#f6931f";	
 inp.style.fontWeight="bold";
 
 //add everthing to the paragraoh
@@ -68,10 +68,7 @@ $( "#slider"+arr[itemNum][1] ).slider({
 		slide: function( event, ui ) {	
 			arr[itemNum][37] = ui.value;
 			$( "#amount"+arr[itemNum][1] ).val(arr[itemNum][37] + " " + units);
-			var parent=document.getElementById("plantLayout");
-			var child=document.getElementById("garden");
-			parent.removeChild(child);
-			plantGardenStraight();
+			redrawGarden();
 			//( ui.value + " " + units);
 		}
 	});
@@ -93,6 +90,15 @@ function drawList()
 		makeSlider(window.list, x, 'lbs', 0, 50);
 		//alert("Data: " + x);
 		}
+}
+
+function redrawList()
+{
+	//removes list and redraws list
+	var parent=document.getElementById("vegList");
+	var child=document.getElementById("list");
+	parent.removeChild(child);
+	drawList();
 }
 
  function addPlant(id)
@@ -138,16 +144,14 @@ function drawList()
 			}
 			//adds default quantity
 			m.push(10);
+			m.push(1);
 			//alert("Data: " + m);
 			//adds vegitable to array
 			window.list.push(m);
 		}
 		
-		//removes list and redraws list
-		var parent=document.getElementById("vegList");
-		var child=document.getElementById("list");
-		parent.removeChild(child);
-		drawList();
+		redrawList();
+		redrawGarden();
 	});
  }
  
@@ -162,11 +166,8 @@ function drawList()
 			window.list.splice(x,1);
 		}
 	}
-	//removes list and redraws list
-	var parent=document.getElementById("vegList");
-	var child=document.getElementById("list");
-	parent.removeChild(child);
-	drawList();
+	redrawList();
+	redrawGarden();
  }
 
  function getPlants(){
@@ -187,4 +188,109 @@ function drawList()
 		//you need to place a call out to the function that uses this
 		alert("Data: " + rows[1][1]);
 	});
+}
+
+function makeSliderObj(obj)
+{
+//create vegitable contanier div
+var div=document.createElement("div");
+//alert(obj.name);
+div.id=obj.name;
+
+//make paragraph for label
+var para=document.createElement("p");
+//make label
+var lab=document.createElement("label");
+lab.htmlFor="amount"+obj.name;
+var node1=document.createTextNode(obj.name+":");
+
+//make input
+var inp=document.createElement("input");
+inp.type="text";
+inp.id="amount"+obj.name;
+inp.style.border="0"; 
+inp.style.color="#f6931f";	
+inp.style.fontWeight="bold";
+
+//add everthing to the paragraoh
+lab.appendChild(node1);
+para.appendChild(lab);
+para.appendChild(inp);
+
+//make slider div
+var div1=document.createElement("div");
+div1.id="slider"+obj.name;
+
+//make paragraph for min max label
+var para2=document.createElement("p");
+
+//make min label
+var lab2=document.createElement("label");
+//lab.htmlFor="amount"+obj.name;
+var node1=document.createTextNode(obj.min);
+
+var para3=document.createElement("p");
+para3.style.textAlign="right";
+//make max label
+var lab3=document.createElement("label");	
+var node2=document.createTextNode(obj.max);
+
+	
+lab2.appendChild(node1);
+lab3.appendChild(node2);
+
+para2.appendChild(lab2);
+para3.appendChild(lab3);
+
+div1.appendChild(para2);
+div1.appendChild(para3);
+
+//add everything to the vegitable contanier div
+div.appendChild(para);
+div.appendChild(div1);
+
+//add vegitable contanier to vegList
+var element=document.getElementById("siteList");
+element.appendChild(div);
+
+$( "#slider"+obj.name ).slider({
+		//range: true,
+		min: obj.min,
+		max: obj.max,
+		step: obj.step,
+		value: obj.value,
+		slide: function( event, ui ) {	
+			obj.value = ui.value;
+			$( "#amount"+obj.name).val(obj.value + " " + obj.units);
+			calculateHealth();
+			redrawGarden();
+			//( ui.value + " " + units);
+		}
+	});
+	$( "#amount"+obj.name).val( $( "#slider"+obj.name).slider( "values", 0 ) + " " + obj.units );
+}
+
+function calculateHealth()
+{
+	//get site values
+	//get the total amount of water from irragation and rain fall as number of days to get an inch
+	var water = 1/((window.site.rainfall/360)+(1/window.site.irragate.value));
+	var ph=window.site.ph.value;
+	
+	//alert("Data: " + ph);
+	for(x in window.list)
+		{
+		var health = 1;
+		health = health*(water<window.list[x][4])?water/window.list[x][4]:window.list[x][4]/water;
+		//alert("Data: " + x +" "+ health);
+		
+		var y;
+		y=window.list[x][24];
+		
+		//save health to the array
+		window.list[x][38]=health;
+	
+		//alert("Data:");
+		//window.document.getElementsByClassName("Onion").style.fill = colorToHex('rgb(0, 0, 0)');
+		}
 }
