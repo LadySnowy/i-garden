4e582dc454600d5e60b04cc484c026da84611b54
TC.Johnism@gmail.com <TC.Johnism@gmail.com@4e8364fe-7fed-9134-efd9-740170bc0cd8>
2012-11-19 18:42:30 +0000

    changed to handle vegetables as objects

--- src/plantGarden.js
+++ src/plantGarden.js
@@ -1,14 +1,12 @@
 function plantGardenStraight()
 {	
 	//create site
-	//<svg height=site.lenght*24 width=site.width*24 xmlns="http://www.w3.org/2000/svg">
 	var plot=document.createElementNS("http://www.w3.org/2000/svg", "svg");
 	plot.setAttribute("height", window.site.lenght*24);
 	plot.setAttribute("width", window.site.width*24);
 	plot.id="garden";
 	
 	//create rectangle
-	//<rect x="0" y="0" width=site.width*24 height=site.lenght*24 style="fill:brown;" />
 	var rec1=document.createElementNS("http://www.w3.org/2000/svg", "rect");
 	rec1.setAttribute("x", 0);
 	rec1.setAttribute("y", 0);
@@ -16,38 +14,50 @@ function plantGardenStraight()
 	rec1.setAttribute("height", window.site.lenght*24);
 	rec1.style.fill = '#83512D';
 	
+	//add the rectangle
 	plot.appendChild(rec1);
 	
+	//set the first position
 	var locx=0;
-	var locy=parseInt(window.list[0][9]);
-	for(x in list)
+	var locy=parseInt(window.list[0].min_spacing);
+	//go thru each vegitable
+	for(x in window.list)
 	{
+	//alert(window.list[x].number);
 		//get health number
-		var h = 160-(window.list[x][38]*160);
+		var h = 160-(window.list[x].health*160);
 		//calculate color from health
 		var color = "#"+((((1 << 24)+(h << 16)+(160 << 8)+h)).toString(16)).slice(1,7);
-		//alert("Data: " + color);
-		for(var i=0; i<window.list[x][37]; i++)
+		
+		//go thru each plant		
+		for(var i=0; i<parseInt(window.list[x].number); i++)
 		{
 			//create circle
 			//<circle id="greencircle" cx="30" cy="30" r="15" fill="green" />
 			var cir1=document.createElementNS("http://www.w3.org/2000/svg", "circle");
-			locx=locx+parseInt(window.list[x][9]);
-			if(locx+parseInt(window.list[x][9])>window.site.width*24)
+			
+			//set x value
+			locx=locx+parseInt(window.list[x].min_spacing);
+			//alert("Data: " + window.site.width*24);
+			//check it row is full
+			if(locx+parseInt(window.list[x].min_spacing)>parseInt(window.site.width)*24)
 			{
-				locx=parseInt(window.list[x][9]);
-				locy=locy+(parseInt(window.list[x][9])*2);
+				//alert("Data: " + window.list[x].min_spacing);	
+				locx=parseInt(window.list[x].min_spacing);
+				locy=locy+(parseInt(window.list[x].min_spacing)*2);
 			}
 			cir1.setAttribute("cx", locx);
-			locx=locx+parseInt(window.list[x][9]);
+			locx=locx+parseInt(window.list[x].min_spacing);
+			
+			//set y value
 			cir1.setAttribute("cy", locy);
-			cir1.setAttribute("r", parseInt(window.list[x][9]));
-			cir1.setAttribute("class", window.list[x][1]);
+			cir1.setAttribute("r", parseInt(window.list[x].min_spacing));
+			cir1.setAttribute("class", window.list[x].name);
 			//alert("Data: " + window.list[0][38]);
-			var h = 160-(window.list[x][38]*160);			
 			cir1.style.fill = color;
 		
 			plot.appendChild(cir1);
+		
 		}
 	}
 	
