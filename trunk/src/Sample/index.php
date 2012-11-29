aa03e3039789bcb818515c4208db241d57f55ea2
clee0718@gmail.com <clee0718@gmail.com@4e8364fe-7fed-9134-efd9-740170bc0cd8>
2012-11-29 03:07:13 +0000

    Some update on the tree graph

--- trunk/src/Sample/index.php
+++ trunk/src/Sample/index.php
@@ -6,82 +6,169 @@
 <script src="js/d3.v2.min.js"></script>
 <script>
 	
-	
-    
-	
-	//Setting up svg
-	var svgw = 700;
-	var svgh = 300;
-    
        
    
-   //Get data
+   /**
+	*   Data
+	*
+	*/
    var data = [{"name":"Carrots","water":30,"sun":40,"soil":150,"com":"Test"},
    				{"name":"Test","water":50,"sun":60,"soil":180,"com":"Carrots"},
    				{"name":"AppleTree","water":40,"sun":20,"soil":120,"com":null}];
-   				
-   				 
-   var steps = 100;
-      
-   var svg = d3.select("body").append("svg");
+   
+   
+   var attrs = new Array();
+   attrs[0] = "name";
+   attrs[1] = "com";
+   attrs[2] = "water";
+   attrs[3] = "sun";
+   attrs[4] = "soil";
+   
+   var translateX = 250;
+   var translateY = 250;
+   		
+   /**
+	*   Initial setup
+	*
+	*/				 
+   	var steps = 100;
+	var svgw = 700;
+	var svgh = 300;   
+	var svg = d3.select("body").append("svg");
    
    var g=svg.selectAll("g").data(data).enter()
   .append("g")
-  .attr("transform",function(d,i) {return "translate(250, 250)";})
+  .attr("transform",function(d,i) {return "translate("+translateX+", "+translateY+")";})
   .attr("id",function(d){return d.name;});
   
-   g.on("mouseover", function(d) {g.selectAll("#"+d.name+" circle").style("opacity","1")
-   									g.selectAll("#"+d.name+" text").style("opacity","1")
-   									d3.select("#"+d.com + " path").style("stroke","green")
-   									d3.select("#"+d.com + " .name").style("opacity","1").style("fill","green")
-   									});
-   g.on("mouseout", function(d) {g.selectAll("#"+d.name+" circle").style("opacity","0")
-   								g.selectAll("#"+d.name+" text").style("opacity","0")
-   								d3.select("#"+d.com + " path").style("stroke","lime")
-   								d3.select("#"+d.com + " .name").style("opacity","0").style("fill","black")});
+  
+   /**
+	*   Draw background grids
+	*
+	*/
+   d3.select("svg").append("line")
+		.attr("class","baseline")
+		.attr("x1",translateX)
+		.attr("y1",translateY+250)
+		.attr("x2",translateX+svgw)
+		.attr("y2",translateY+250)
+		
+	for(var i = 1; i<5; i++){
+	d3.select("svg").append("line")
+		.attr("class","grids")
+		.attr("x1",translateX)
+		.attr("y1",translateY+250 - steps * i)
+		.attr("x2",translateX+svgw)
+		.attr("y2",translateY+250 - steps * i)
+	}
+	
+	for(var i = 2; i<attrs.length; i++){
+	d3.select("svg").append("text")
+		.attr("class","gridtext")
+   		.attr("x",translateX+svgw+10)
+   		.attr("y",translateY+250 - steps * (i-1))
+		.text(attrs[i]);
+	}
+		
+  
+   /**
+	*   Mouse events
+	*
+	*/
+   g.on("mouseover", function(d) {	
    
+   	//Change attributes of all vegetables
+   	g.selectAll("path").style("opacity","0.2")
+	
+	//Change attributes of selected vegetable								
+	g.selectAll("#"+d.name+" circle").style("opacity","1")
+	g.selectAll("#"+d.name+" text").style("opacity","1")
+	g.select("#"+d.name + " path").style("opacity","1")
+	g.select("#"+d.name + " path").style("stroke","green")
+	
+	//Change attributes of XXX plants
+	g.select("#"+d.com + " path").style("stroke","red")
+	g.select("#"+d.com + " .name").style("opacity","1").style("fill","red")
+	});
+
+   g.on("mouseout", function(d) {
+   
+    //Change all vegetables back
+   	g.selectAll("path").style("opacity","1")
+   	
+   	//Change the selected vegetable back
+	g.selectAll("#"+d.name+" circle").style("opacity","0")
+	g.selectAll("#"+d.name+" text").style("opacity","0")
+	g.select("#"+d.name + " path").style("stroke","lime")
+	
+	//Change the XXX back
+	g.select("#"+d.com + " path").style("stroke","lime")
+	g.select("#"+d.com + " .name").style("opacity","0").style("fill","black")
+	});
+	
+   
+   /**
+	*   Draw initial paths
+	*
+	*/
    g
   .append("path")
-  .attr("d",function(d){return "m 250 250 l " + d.water + " -" + steps + " l " + d.sun + " -" + steps + " l " + d.soil + " -" + steps })
+  .attr("d",function(d){return "m 250 250 l " + d[attrs[2]] + " -" + steps + " l " + d[attrs[3]] + " -" + steps + " l " + d[attrs[4]] + " -" + steps })
   
+  
+  
+	/**
+	*   Dots creation
+	*
+	*/		  
+   var radius = 3;
    g
    .append("circle")
-   		.attr("cx",function(d){return 250 + d.water})
+   		.attr("cx",function(d){return 250 + d[attrs[2]]})
    		.attr("cy",function(d){return 250 - steps})
-   		.attr("r",function(d){return 3})
+   		.attr("r",function(d){return radius})
    g.append("circle")
-   		.attr("cx",function(d){return 250 + d.water + d.sun})
+   		.attr("cx",function(d){return 250 + d[attrs[2]] + d[attrs[3]]})
    		.attr("cy",function(d){return 250 - steps * 2})
-   		.attr("r",function(d){return 3});
+   		.attr("r",function(d){return radius});
    g.append("circle")
-   		.attr("cx",function(d){return 250 + d.water + d.sun + d.soil})
+   		.attr("cx",function(d){return 250 + d[attrs[2]] + d[attrs[3]] + d[attrs[4]]})
    		.attr("cy",function(d){return 250 - steps * 3})
-   		.attr("r",function(d){return 3});
-   		
+   		.attr("r",function(d){return radius});
+   	
+   	
+   	/**
+	*   Attribute texts creation
+	*
+	*/		
    g
    .append("text")
-   		.attr("x",function(d){return 250 + d.water + 5})
+   		.attr("x",function(d){return 250 + d[attrs[2]] + 5})
    		.attr("y",function(d){return 250 - steps})
-   		.text(function(d){return d.water})
+   		.text(function(d){return d[attrs[2]]})
    g.append("text")
-   		.attr("x",function(d){return 250 + d.water + d.sun + 5})
+   		.attr("x",function(d){return 250 + d[attrs[2]] + d[attrs[3]] + 5})
    		.attr("y",function(d){return 250 - steps * 2})
-   		.text(function(d){return d.sun});
+   		.text(function(d){return d[attrs[3]]});
    g.append("text")
-   		.attr("x",function(d){return 250 + d.water + d.sun + d.soil + 5})
+   		.attr("x",function(d){return 250 + d[attrs[2]] + d[attrs[3]] + d[attrs[4]] + 5})
    		.attr("y",function(d){return 250 - steps * 3})
-   		.text(function(d){return d.soil});
-   		
+   		.text(function(d){return d[attrs[4]]});
+   	
+   	/**
+	*   Attribute name creation
+	*
+	*/		
    g
    .append("text")
-   		.attr("x",function(d){return 250 + d.water + d.sun + d.soil + 5})
+   		.attr("x",function(d){return 250 + d[attrs[2]] + d[attrs[3]] + d[attrs[4]] + 5})
    		.attr("y",function(d){return 250 - steps * 3 - 25})
    		.attr("class","name")
-   		.text(function(d){return d.name})
+   		.text(function(d){return d[attrs[0]]})
   
   
   
-   
+    
 
 	    
 
